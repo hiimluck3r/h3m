ce844398ff42088b4d6cbde4d6c95568a811bf9c
hiimluck3r <luck3rinc@yandex.ru>
2024-08-01 16:00:34 +0300

    migrate from kong to cilium

--- cilium_config.tf
+++ cilium_config.tf
@@ -2,9 +2,10 @@ data "helm_template" "cilium_template" {
   name = "cilium"
   namespace = "kube-system"
   repository = "https://helm.cilium.io/"
+  kube_version = "1.21.0-0"
 
   chart = "cilium"
-  version = "1.15.3"
+  version = "1.16.0"
 
   include_crds = true
 
@@ -13,9 +14,32 @@ data "helm_template" "cilium_template" {
       mode: kubernetes
     kubeProxyReplacement: true
     
+    ingressController:
+      enabled: true
+      default: true
+    
+    gatewayAPI:
+      enabled: true
+      hostNetwork:
+        enabled: false
+      gatewayClass:
+        create: "true"
+      externalTrafficPolicy: "Local"
+
+    envoy:
+      enabled: true
+      securityContext:
+        capabilities:
+          keepCapNetBindService: true
+          envoy:
+          # Add NET_BIND_SERVICE to the list (keep the others!)
+          - NET_BIND_SERVICE
+          - NET_ADMIN
+          - SYS_ADMIN
+
     l2announcements:
       enabled: true
-    l7Proxy: false
+    l7Proxy: true
 
     securityContext:
       privileged: true
@@ -52,7 +76,7 @@ data "helm_template" "cilium_template" {
         enabled: true
         rollOutPods: true
       ui:
-        enabled: true
+        enabled: false
 
     cgroup:
       autoMount:
